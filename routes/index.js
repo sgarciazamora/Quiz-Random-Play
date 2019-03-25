700711c18a0d349e8544599403754ddcdb7396c0
Santiago Pavon <santiago@dit.upm.es>
2019-03-25 15:17:58 +0100

    Step 2 - Change home page title, remove User resource and add favicon, Add Author page.

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res, next) => {
+  res.render('index', { title: 'Quiz' });
+});
+
+// Author page.
+router.get('/author', (req, res, next) => {
+  res.render('author');
 });
 
 module.exports = router;
